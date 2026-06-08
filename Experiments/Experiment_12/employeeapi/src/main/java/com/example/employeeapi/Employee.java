a76887da17b3e3aeb5f8b9e56656fe67b088d721
tusharkandoo <tusharkandoo@gmail.com>
2026-06-08 20:31:29 +0530

    Add Experiments/Experiment_12/employeeapi/src/main/java/com/example/employeeapi/Employee.java

---
+++ Experiments/Experiment_12/employeeapi/src/main/java/com/example/employeeapi/Employee.java
@@ -0,0 +1,22 @@
+package com.example.employeeapi;
+import jakarta.persistence.*;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String department;
+    private double salary;
+
+    public double getSalary() { return salary; }
+    public void setSalary(double salary) { this.salary = salary; }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
+}
